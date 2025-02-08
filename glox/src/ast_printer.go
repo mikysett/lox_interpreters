26b746b45a97ef8fd910c958e86ef19731000e04
Michele Sessa <michele.sessa@freiehit.com>
2025-02-08 17:02:18 +0000

    implement ast printer

---
+++ glox/src/ast_printer.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type AstPrinter struct{}
+
+func (ast *AstPrinter) print(expr Expr) any {
+	return expr.accept(ast)
+}
+
+func (ast *AstPrinter) visitBinaryExpr(expr *ExprBinary) any {
+	return ast.parenthesize(expr.operator.Lexeme, expr.left, expr.right)
+}
+
+func (ast *AstPrinter) visitGroupingExpr(expr *ExprGrouping) any {
+	return ast.parenthesize("group", expr.expression)
+}
+
+func (ast *AstPrinter) visitLiteralExpr(expr *ExprLiteral) any {
+	if expr.value == nil {
+		return "nil"
+	}
+	return fmt.Sprintf("%v", expr.value)
+}
+
+func (ast *AstPrinter) visitUnaryExpr(expr *ExprUnary) any {
+	return ast.parenthesize(expr.operator.Lexeme, expr.right)
+}
+
+func (ast *AstPrinter) parenthesize(name string, exprs ...Expr) string {
+	var builder strings.Builder
+	builder.WriteByte('(')
+	builder.WriteString(name)
+	for _, expr := range exprs {
+		builder.WriteByte(' ')
+		if v, ok := expr.accept(ast).(string); ok {
+			builder.WriteString(v)
+		} else {
+			panic("Unreachable non string return in AstPrinter!")
+		}
+	}
+	builder.WriteByte(')')
+	return builder.String()
+}
