b2c8f0e6823989e6e8aedc9f917d035258e1d5cf
Michele Sessa <michele.sessa@freiehit.com>
2025-02-15 17:27:13 +0000

    add enviroment for persistent state in the interpreter

---
+++ glox/src/environment.go
@@ -0,0 +1,23 @@
+package main
+
+type Environment struct {
+	values map[string]any
+}
+
+func NewEnvironment() *Environment {
+	return &Environment{
+		values: map[string]any{},
+	}
+}
+
+func (env *Environment) define(k string, v any) {
+	env.values[k] = v
+}
+
+func (env *Environment) get(name Token) (any, error) {
+	v, ok := env.values[name.Lexeme]
+	if !ok {
+		return nil, NewRuntimeError(&name, "Undefined variable '"+name.Lexeme+"'.")
+	}
+	return v, nil
+}
