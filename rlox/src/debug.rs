e4ef8b7a5b6aaa04ac9313aa069b59fc83a682e8
Michele Sessa <mikysett@gmail.com>
2025-08-11 23:34:33 +0100

    refactor debug to be behind a feature flag

--- rlox/src/debug.rs
+++ rlox/src/debug.rs
@@ -1,55 +1,49 @@
-use crate::domain::Chunk;
-use crate::domain::OpCode;
+#[cfg(debug)]
+pub mod debug {
+    use crate::domain::Chunk;
+    use crate::domain::OpCode;
 
-pub fn disassemble_chunk(chunk: &Chunk, name: &str) {
-    println!("== {name} ==");
+    pub fn disassemble_instruction(chunk: &Chunk, offset: usize) -> usize {
+        print!("{:04} ", offset);
 
-    let mut offset = 0;
-    while offset < chunk.code.len() {
-        offset = disassemble_instruction(chunk, offset);
-    }
-}
+        let current_line = chunk.get_line(offset);
+        if offset > 0 && chunk.get_line(offset - 1) == current_line {
+            print!("   | ");
+        } else {
+            print!("{:4} ", current_line);
+        }
 
-fn disassemble_instruction(chunk: &Chunk, offset: usize) -> usize {
-    print!("{:04} ", offset);
-
-    let current_line = chunk.get_line(offset);
-    if offset > 0 && chunk.get_line(offset - 1) == current_line {
-        print!("   | ");
-    } else {
-        print!("{:4} ", current_line);
-    }
-
-    match OpCode::from(chunk.code[offset]) {
-        OpCode::OpReturn => simple_instruction("OP_RETURN", offset),
-        OpCode::OpConstant => constant_instruction("OP_CONSTANT", offset, chunk),
-        OpCode::OpConstantLong => constant_long_instruction("OP_CONSTANT_LONG", offset, chunk),
-        OpCode::Unknown => {
-            println!("Unknown opcode {} at offset {}", chunk.code[offset], offset);
-            offset + 1
+        match OpCode::from(chunk.code[offset]) {
+            OpCode::OpReturn => simple_instruction("OP_RETURN", offset),
+            OpCode::OpConstant => constant_instruction("OP_CONSTANT", offset, chunk),
+            OpCode::OpConstantLong => constant_long_instruction("OP_CONSTANT_LONG", offset, chunk),
+            OpCode::OpNegate => simple_instruction("OP_NEGATE", offset),
+            OpCode::Unknown => {
+                println!("Unknown opcode {} at offset {}", chunk.code[offset], offset);
+                offset + 1
+            }
         }
     }
-}
 
-fn simple_instruction(name: &str, offset: usize) -> usize {
-    println!("{name}");
-    offset + 1
-}
+    fn simple_instruction(name: &str, offset: usize) -> usize {
+        println!("{name}");
+        offset + 1
+    }
 
-fn constant_instruction(name: &str, offset: usize, chunk: &Chunk) -> usize {
-    let pointer = chunk.code[offset + 1] as usize;
-    print!("{name:-16} {:4} '", pointer);
-    chunk.constants[pointer].print();
-    println!("'");
-    offset + 2
-}
+    fn constant_instruction(name: &str, offset: usize, chunk: &Chunk) -> usize {
+        let pointer = chunk.code[offset + 1] as usize;
+        println!("{name:-16} {:4} '{}'", pointer, chunk.constants[pointer]);
+        offset + 2
+    }
 
-fn constant_long_instruction(name: &str, offset: usize, chunk: &Chunk) -> usize {
-    let pointer = chunk.code[offset + 1] as usize
-        | (chunk.code[offset + 2] as usize) << 8
-        | (chunk.code[offset + 3] as usize) << 16;
-    print!("{name:-16} {:4} '", pointer);
-    chunk.constants[pointer as usize].print();
-    println!("'");
-    offset + 4
+    fn constant_long_instruction(name: &str, offset: usize, chunk: &Chunk) -> usize {
+        let pointer = chunk.code[offset + 1] as usize
+            | (chunk.code[offset + 2] as usize) << 8
+            | (chunk.code[offset + 3] as usize) << 16;
+        println!(
+            "{name:-16} {:4} '{}'",
+            pointer, chunk.constants[pointer as usize]
+        );
+        offset + 4
+    }
 }
