801e27c2ae44f5b5e4814e5b45d0b5ce8f38336b
Michele Sessa <mikysett@gmail.com>
2025-08-31 12:11:50 +0100

    add opCodes and improve naming

--- rlox/src/domain/chunk.rs
+++ rlox/src/domain/chunk.rs
@@ -3,30 +3,51 @@ use crate::domain::value::Value;
 #[derive(Debug)]
 #[repr(u8)]
 pub enum OpCode {
-    OpReturn = 0,
-    OpConstant,
-    OpConstantLong,
-    OpAdd,
-    OpSubtract,
-    OpMultiply,
-    OpDivide,
-    OpNegate,
+    Return = 0,
+    Constant = 1,
+    True = 2,
+    False = 3,
+    Nil = 4,
+    ConstantLong = 5,
+    Add = 6,
+    Subtract = 7,
+    Multiply = 8,
+    Divide = 9,
+    Negate = 10,
+    Not = 11,
+    Equal = 12,
+    Greater = 13,
+    Less = 14,
     Unknown = 0xff,
 }
 
+// #[cfg(debug)]
 impl From<u8> for OpCode {
     fn from(value: u8) -> Self {
         match value {
-            0 => OpCode::OpReturn,
-            1 => OpCode::OpConstant,
-            2 => OpCode::OpConstantLong,
-            3 => OpCode::OpAdd,
-            4 => OpCode::OpSubtract,
-            5 => OpCode::OpMultiply,
-            6 => OpCode::OpDivide,
-            7 => OpCode::OpNegate,
+            0 => OpCode::Return,
+            1 => OpCode::Constant,
+            2 => OpCode::True,
+            3 => OpCode::False,
+            4 => OpCode::Nil,
+            5 => OpCode::ConstantLong,
+            6 => OpCode::Add,
+            7 => OpCode::Subtract,
+            8 => OpCode::Multiply,
+            9 => OpCode::Divide,
+            10 => OpCode::Negate,
+            11 => OpCode::Not,
+            12 => OpCode::Equal,
+            13 => OpCode::Greater,
+            14 => OpCode::Less,
             _ => OpCode::Unknown,
         }
+    }
+}
+
+impl Into<u8> for OpCode {
+    fn into(self) -> u8 {
+        self as u8
     }
 }
 
@@ -80,12 +101,12 @@ impl Chunk {
         let index = self.add_constant(value);
 
         if index >= u8::MAX as usize {
-            self.write(OpCode::OpConstantLong as u8, line);
+            self.write(OpCode::ConstantLong as u8, line);
             self.write((index & 0xff) as u8, line);
             self.write(((index >> 8) & 0xff) as u8, line);
             self.write(((index >> 16) & 0xff) as u8, line);
         } else {
-            self.write(OpCode::OpConstant as u8, line);
+            self.write(OpCode::Constant as u8, line);
             self.write(index as u8, line);
         }
     }
