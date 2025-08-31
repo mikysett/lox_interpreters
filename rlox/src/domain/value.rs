de56a1fbf0e98ab943631e5e4e2e1da8d16038bf
Michele Sessa <mikysett@gmail.com>
2025-08-31 12:10:59 +0100

    implement helpers for Value comparisons

--- rlox/src/domain/value.rs
+++ rlox/src/domain/value.rs
@@ -4,12 +4,35 @@ use std::fmt;
 pub enum Value {
     Unknown = 0,
     Double(f64),
+    Bool(bool),
+    Nil,
+}
+
+impl Value {
+    pub fn is_falsey(&self) -> bool {
+        match self {
+            Value::Nil => true,
+            Value::Bool(false) => true,
+            _ => false,
+        }
+    }
+
+    pub fn equal(&self, other: &Value) -> bool {
+        match (self, other) {
+            (Value::Bool(a), Value::Bool(b)) => a == b,
+            (Value::Nil, Value::Nil) => true,
+            (Value::Double(a), Value::Double(b)) => a == b,
+            _ => false,
+        }
+    }
 }
 
 impl fmt::Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Value::Double(n) => write!(f, "{n}"),
+            Value::Bool(n) => write!(f, "{n}"),
+            Value::Nil => write!(f, "nil"),
             Value::Unknown => write!(f, "unknown"),
         }
     }
